d0295be7c0874b47a3bec6be23f697309e428fbb
Slava <Slava7711240@gmail.com>
2023-01-18 19:10:49 +0300

    improved MainTableView and cell

--- Weather/View/MainView/Cells/MainTableViewCell.swift
+++ Weather/View/MainView/Cells/MainTableViewCell.swift
@@ -9,15 +9,70 @@ import UIKit
 
 final class MainTableViewCell: UITableViewCell {
 
+    // MARK: - Constants
+
+    private enum Constants {
+        static let temperatureSign = "°C"
+    }
+
     // MARK: - IBOutlets
 
     @IBOutlet private weak var nameCityLabel: UILabel!
     @IBOutlet private weak var statusCityLabel: UILabel!
     @IBOutlet private weak var tempCityLabel: UILabel!
+    @IBOutlet private weak var tempConstantLabel: UILabel!
+    
+    // MARK: - UITableViewCell
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupInitialState()
+    }
+    
+    // MARK: - Internal Methods
+    
+    func setupInitialState() {
+        configureNameCityLabel()
+        configureStatusCityLabel()
+        configureTempCityLabel()
+        configureTempConstantLabel()
+    }
 
     func configure(weather: Weather) {
         self.nameCityLabel.text = weather.name
         self.statusCityLabel.text = weather.conditionString
         self.tempCityLabel.text = weather.temperatureString
     }
+
+}
+
+// MARK: - Private Methods
+
+private extension MainTableViewCell {
+
+    func configureNameCityLabel() {
+        nameCityLabel.textColor = AssetColor.black
+        nameCityLabel.font = UIFont.systemFont(ofSize: 17, weight: .regular)
+        nameCityLabel.textAlignment = .left
+    }
+
+    func configureStatusCityLabel() {
+        statusCityLabel.textColor = AssetColor.gray
+        statusCityLabel.font = UIFont.systemFont(ofSize: 14, weight: .light)
+        statusCityLabel.textAlignment = .right
+    }
+
+    func configureTempCityLabel() {
+        tempCityLabel.textColor = AssetColor.black
+        tempCityLabel.font = UIFont.systemFont(ofSize: 17, weight: .regular)
+        tempCityLabel.textAlignment = .right
+    }
+
+    func configureTempConstantLabel() {
+        tempConstantLabel.text = Constants.temperatureSign
+        tempConstantLabel.textColor = AssetColor.black
+        tempConstantLabel.font = UIFont.systemFont(ofSize: 17, weight: .regular)
+        tempConstantLabel.textAlignment = .right
+    }
+
 }
--- Weather/View/MainView/MainTableViewController.swift
+++ Weather/View/MainView/MainTableViewController.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import CoreLocation
 
 final class MainTableViewController: UITableViewController {
 
@@ -14,66 +13,52 @@ final class MainTableViewController: UITableViewController {
 
     let emptyCity = Weather()
     let userServise = UserService()
-    var citiesArray = [Weather]()
-    var nameCities = [
-        "Antalya",
-        "Buenos Aires",
-        "Helsinki",
-        "Ottawa",
-        "Berlin",
-        "Milan",
-        "Tokyo",
-        "Moscow",
-        "London"
-    ]
 
+    var citiesArray = [Weather]()
+    var filterCityArray = [Weather]()
+    var nameCities = ["Antalya", "Buenos Aires", "Helsinki", "Ottawa", "Berlin", "Milan", "Tokyo", "Moscow", "London"]
+
+    // MARK: - Private Properties
+
+    private let searchController = UISearchController(searchResultsController: nil)
+    private var searchBarIsEmpty: Bool {
+        guard let text = searchController.searchBar.text else {
+            return false
+        }
+        return text.isEmpty
+    }
+    private var isFiltering: Bool {
+        return searchController.isActive && !searchBarIsEmpty
+    }
+    
     // MARK: - UITableViewController
 
     override func viewDidLoad() {
         super.viewDidLoad()
         configureAppearance()
-
         checkCitiesArray()
         addCities()
-    }
-    
-    func checkCitiesArray() {
-        if citiesArray.isEmpty {
-            citiesArray = Array(repeating: emptyCity, count: nameCities.count)
-        }
-    }
-    
-    func addCities() {
-        getCityWeather(citiesArray: self.nameCities) { (index, weather) in
-            self.citiesArray[index] = weather
-            self.citiesArray[index].name = self.nameCities[index]
-            print(self.citiesArray)
-            
-            DispatchQueue.main.async {
-                self.tableView.reloadData()
-            }
-        }
-    }
-    
-    @IBAction func didTapAddCity(_ sender: Any) {
-        alertAddCity(name: "City", placeholder: "Enter the name of the city") { (city) in
-            self.nameCities.append(city)
-            self.citiesArray.append(self.emptyCity)
-            self.addCities()
-        }
     }
 
     // MARK: - UITableViewDataSource
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if isFiltering {
+            return filterCityArray.count
+        }
         return citiesArray.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! MainTableViewCell
         var weather = Weather()
-        weather = citiesArray[indexPath.row]
+        if isFiltering {
+            weather = filterCityArray[indexPath.row]
+        } else {
+            weather = citiesArray[indexPath.row]
+        }
         cell.configure(weather: weather)
+        cell.backgroundColor = AssetColor.blue
         return cell
     }
 
@@ -82,9 +67,15 @@ final class MainTableViewController: UITableViewController {
             guard let indexPath = tableView.indexPathForSelectedRow else {
                 return
             }
-            let cityWeather = citiesArray[indexPath.row]
-            let detailVC = segue.destination as! DetailViewController
-            detailVC.weatherModel = cityWeather
+            if isFiltering {
+                let filter = filterCityArray[indexPath.row]
+                let detailVC = segue.destination as! DetailViewController
+                detailVC.weatherModel = filter
+            } else {
+                let cityWeather = citiesArray[indexPath.row]
+                let detailVC = segue.destination as! DetailViewController
+                detailVC.weatherModel = cityWeather
+            }
         }
     }
 
@@ -92,7 +83,11 @@ final class MainTableViewController: UITableViewController {
         let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { (_, _, completionHandler) in
             let editingRow = self.nameCities[indexPath.row]
             if let index = self.nameCities.firstIndex(of: editingRow) {
-                self.citiesArray.remove(at: index)
+                if self.isFiltering {
+                    self.filterCityArray.remove(at: index)
+                } else {
+                    self.citiesArray.remove(at: index)
+                }
             }
             tableView.reloadData()
         }
@@ -107,6 +102,8 @@ private extension MainTableViewController {
 
     func configureAppearance() {
         configureLottieAnimation()
+        configureSearchController()
+        configureTableView()
     }
 
     func configureLottieAnimation() {
@@ -114,4 +111,69 @@ private extension MainTableViewController {
         LottieLaunchScreen().configureLottieAnimation(on: controller.view)
     }
 
+    func configureSearchController() {
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search"
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
+        navigationItem.hidesSearchBarWhenScrolling = false
+        searchController.searchBar.backgroundColor = AssetColor.blue
+    }
+
+    func configureTableView() {
+        self.title = "Weather"
+        tableView.backgroundColor = AssetColor.blue
+        tableView.alwaysBounceVertical = false
+    }
+
+    func checkCitiesArray() {
+        if citiesArray.isEmpty {
+            citiesArray = Array(repeating: emptyCity, count: nameCities.count)
+        }
+    }
+
+    func addCities() {
+        CityWeather.getCityWeather(citiesArray: self.nameCities) { (index, weather) in
+            self.citiesArray[index] = weather
+            self.citiesArray[index].name = self.nameCities[index]
+            print(self.citiesArray)
+            
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+}
+
+// MARK: - Internal Methods
+
+extension MainTableViewController: UISearchResultsUpdating {
+
+    func updateSearchResults(for searchController: UISearchController) {
+        filterContentForSearchText(searchController.searchBar.text!)
+    }
+    
+    func filterContentForSearchText(_ searchText: String) {
+        filterCityArray = citiesArray.filter {
+            $0.name.contains(searchText)
+        }
+        tableView.reloadData()
+    }
+
+}
+
+// MARK: - Actions
+
+private extension MainTableViewController {
+
+    @IBAction func didTapAddCity(_ sender: Any) {
+        alertAddCity(name: "City", placeholder: "Enter the name of the city") { (city) in
+            self.nameCities.append(city)
+            self.citiesArray.append(self.emptyCity)
+            self.addCities()
+        }
+    }
+    
 }
